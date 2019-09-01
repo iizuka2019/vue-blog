cce79652fd175824afbb006182e76800e355eec6
iizuka2019 <49476247+iizuka2019@users.noreply.github.com>
2019-09-01 15:58:49 +0900

    Update dev.env.js

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV       : '"development"',
-  BUTTER_API_KEY : '18c4bff3db8974602b7c13d114db1f40ccf76feb',
+  BUTTER_API_KEY : '"18c4bff3db8974602b7c13d114db1f40ccf76feb"',
 })
