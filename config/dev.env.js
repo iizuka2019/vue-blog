f5baf402095a1955108bf1c5d7ea6136ecf02248
iizuka2019 <49476247+iizuka2019@users.noreply.github.com>
2019-09-01 16:09:21 +0900

    Update dev.env.js

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,6 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV       : '"development"',
-  BUTTER_API_KEY : '"18c4bff3db8974602b7c13d114db1f40ccf76feb"',
+  NODE_ENV       : 'development',
+  BUTTER_API_KEY : '18c4bff3db8974602b7c13d114db1f40ccf76feb',
 })
